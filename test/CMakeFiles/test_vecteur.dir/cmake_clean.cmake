0b4c5e4428852df17ccaf68a063a5ecf05746f97
Abel PL <abel.prud-homme-lacroix@grenoble-inp.org>
2025-03-20 12:39:17 +0100

    ajout debut cmake et test

---
+++ test/CMakeFiles/test_vecteur.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_vecteur.dir/test_vecteur.cxx.o"
+  "CMakeFiles/test_vecteur.dir/test_vecteur.cxx.o.d"
+  "test_vecteur"
+  "test_vecteur.pdb"
+  "test_vecteur[1]_tests.cmake"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_vecteur.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
